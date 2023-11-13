ae2d5f93a9eccb0b189f78fbf057db851b9fc4a1
yangwawa0323 <yangwawa0323@163.com>
2023-11-13 16:43:30 +0800

    tailwind background and shadows

--- app/backgrounds-shadows/page.tsx
+++ app/backgrounds-shadows/page.tsx
@@ -2,15 +2,23 @@ import React from 'react';
 
 const BackgroundShadows = () => {
 	return (
-		<div>
+		<div className='space-y-20'>
 			<h2 className=' text-2xl'>Background Shadows</h2>
 			{/* Background Classes */}
+			<div
+				className='h-96 w-96 bg-blue-300 bg-cover bg-right bg-no-repeat'
+				style={{ backgroundImage: 'url("/background-sea.jpg")' }}
+			></div>
 
 			{/* Gradients */}
+			<div className='text-white h-12 w-30 bg-gradient-to-bl from-slate-800 to-red-300'>
+				Click me
+			</div>
 
 			{/* Shadows */}
-
+			<div className='shadow-lg bg-red-400 '>Submit</div>
 			{/* Mix Blend */}
+			<div className='h-20 w-full'></div>
 		</div>
 	);
 };
